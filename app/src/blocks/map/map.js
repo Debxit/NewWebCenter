e97cf273977b8aafd1839ce051682d12364de467
R45H <gmr45h@gmail.com>
2017-06-06 11:49:51 +0300

    Rework contacts page

--- app/src/blocks/map/map.js
+++ app/src/blocks/map/map.js
@@ -5,33 +5,33 @@ function initMap() {
 	if (!$map.length || $map.is(':hidden')) return;
 
 	/* Точки */
-	var dot1 = {lat: 59.999921, lng: 30.256418};
+	var dot1 = {lat: 59.999921, lng: 30.256418}; // Гаккелевская, 21А
+	var dot2 = {lat: 59.999921, lng: 30.229}; // Чуть сбоку от адреса
 	/* ===== */
 
 	/* Карты */
-	if (document.getElementById('map')) {
-		var map1 = new google.maps.Map(document.getElementById('map'), {
+	if (document.getElementById('mapContacts')) { // Карта в контактах
+		var map1 = new google.maps.Map(document.getElementById('mapContacts'), {
 			zoom: 15,
-			center: dot1,
+			center: dot2,
 			scrollwheel: false,
 			mapTypeControl: true,
 			streetViewControl: false,
 			mapTypeControlOptions: {
-				mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE]
+				mapTypeIds: [
+					google.maps.MapTypeId.ROADMAP,
+					google.maps.MapTypeId.SATELLITE
+				]
 			}
 		});
-	}
-	/* ===== */
 
-	/* Маркеры */
-	if (document.getElementById('map')) {
 		var marker1 = new google.maps.Marker({
 			position: dot1,
 			map: map1,
 			title: 'Webcenter',
-			id: 'markerCard-1'
+			id: 'marker1'
 		});
 	}
-	/* ======= */
+	/* ===== */
 }
 /* ========== */
