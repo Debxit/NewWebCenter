d516cd43ad004727b2664f0bc18325375a5f92c4
R45H <gmr45h@gmail.com>
2017-06-13 12:36:49 +0300

    Deactivate jquery-ui. Create my own accordion. Add disabling on dbl click for radiogroup

--- app/src/blocks/accord/accord.js
+++ app/src/blocks/accord/accord.js
@@ -1,3 +1,39 @@
 /* Аккордеон */
-$( ".accord" ).accordion();
+var
+	$accordHead = $('.accord__head'), // Шапки панелей
+	accordOpened = 'accord__panel_opened', // Класс открытой панели
+	accordRadio = 'accord__radio', // Класс чекбокса
+	accordRadioChecked = 'accord__radio_checked', // Класс активного чекбокса
+	$accordRadio = $('.' + accordRadio); // Чекбоксы
+
+$accordHead.on('click', function() {
+	var
+		$this = $(this),
+		$parent = $this.parent(), // Контейнер текущей шапки
+		$next = $this.next(); // Тело панели
+
+	if ($parent.hasClass(accordOpened)) {
+		$parent.removeClass(accordOpened);
+		$next.slideUp(300);
+	} else {
+		$parent.addClass(accordOpened);
+		$next.slideDown(300);
+	}
+});
+
+$accordRadio.on('click', function() {
+	var
+		$this = $(this);
+
+	if ($this.hasClass(accordRadioChecked)) {
+		$this
+			.removeClass(accordRadioChecked)
+			.prop('checked', false);
+	} else {
+		$this.parent().parent().find('.' + accordRadio).removeClass(accordRadioChecked);
+		$this
+			.addClass(accordRadioChecked)
+			.prop('checked', true);
+	}
+});
 /* ========== */
