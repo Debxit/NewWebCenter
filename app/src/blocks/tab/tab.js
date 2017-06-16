71d40ad7ea76f840b0806c0bef7868a230303389
R45H <gmr45h@gmail.com>
2017-06-16 18:02:04 +0300

    Complete desktop version of tabs

--- app/src/blocks/tab/tab.js
+++ app/src/blocks/tab/tab.js
@@ -18,6 +18,7 @@ $tabLink.on('click', function(event) {
 
 	tabToggle = true;
 
+	// Обработка заголовков
 	$tabLink.each(function() {
 		var $thisLink = $(this);
 
@@ -31,7 +32,9 @@ $tabLink.on('click', function(event) {
 				.removeClass(tabLinkAct);
 		}
 	});
+	// =====
 
+	// Обработка табов
 	$tabItem.each(function() {
 		$(this).fadeOut(300, function() {
 			$(this).removeClass(tabItemAct);
@@ -44,5 +47,32 @@ $tabLink.on('click', function(event) {
 			});
 		}, 300);
 	});
+	// =====
+
+	//TODO Сделать перетаскивание ползунка мышкой
+	//TODO Сделать переключение слайдов по таймеру
+	//TODO Сделать поддержку быстрого переключения
+	// Обработка ползунка
+	var
+		$line = $('.tab__line'), // Ползунок
+		$lineRail = $('.tab__line-rail'), // Контейнер ползунка
+		lineRailSpace = parseInt($lineRail.css('margin-left')), // Расстояние контейнера ползунка от края
+		tabCenter = $this.parent().position().left + Math.round($this.width() / 2), // Положение центра текущего заголовка
+		lineCenter = Math.round($line.width() / 2); // Положение центра ползунка
+
+	if (!$this.parent().prev().length) { // Первый заголовок
+		$line.css({
+			'left': 0
+		});
+	} else if (!$this.parent().next().length) { // Последний заголовок
+		$line.css({
+			'left': $lineRail.width() - $line.width() + 1
+		});
+	} else {
+		$line.css({
+			'left': tabCenter - lineCenter - lineRailSpace
+		});
+	}
+	// =====
 });
 /* ========== */
