847c6e280e8d7273e3a73e68c72d1e54aa27733c
R45H <gmr45h@gmail.com>
2017-11-09 09:46:09 +0300

    Rework square-profits

--- app/src/blocks/effective-offer/effective-offer.js
+++ app/src/blocks/effective-offer/effective-offer.js
@@ -1,7 +1,16 @@
-$('.square-profit').click(function () {
-    $('.square-profit').removeClass('active');
-    $(this).addClass('active');
-    var target = $(this).attr('data-target');
-    $('.effective-offer__tab').removeClass('visible');
-    $(target).addClass('visible');
-});
+(function() {
+	var
+		$profits = $('.square-profit'),
+		$effective = $('.effective-offer__tab');
+
+	$profits.on('mouseenter', function() {
+		var
+			$this = $(this),
+			target = $this.attr('data-target');
+
+		$profits.removeClass('active');
+		$this.addClass('active');
+		$effective.removeClass('visible');
+		$(target).addClass('visible');
+	});
+}());
