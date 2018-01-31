e0476650071a3b929ccac5407dac640e369c1f54
R45H <gmr45h@gmail.com>
2018-01-31 16:37:05 +0300

    Some fixes. Rework logo page

--- app/src/blocks/partners/partners.js
+++ app/src/blocks/partners/partners.js
@@ -1,35 +1,39 @@
+/* Слайдер с логотипами клиентов */
 $('.partners').slick({
-    autoplay: true,
-    autoplaySpeed: 7000,
-    // dotsClass: 'customer__dots slick-custom-dots',
-    mobileFirst: true,
-    // prevArrow: '<div class="customer__prev"></div>',
-    // nextArrow: '<div class="customer__next"></div>',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    responsive: [
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
-            }
-        },
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4
-            }
-        }
-    ]
+	autoplay: true,
+	autoplaySpeed: 10000,
+	dotsClass: 'customer__dots slick-custom-dots',
+	mobileFirst: true,
+	prevArrow: '<div class="customer__prev"></div>',
+	nextArrow: '<div class="customer__next"></div>',
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				arrows: false,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+				arrows: false,
+				dots: true
+			}
+		}
+	]
 });
+/* ========== */
