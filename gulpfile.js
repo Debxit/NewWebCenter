6913a311f09baff69460f88db435a3e73b5d15b6
R45H <gmr45h@gmail.com>
2018-02-01 16:38:11 +0300

    Add burevestnik landing

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,8 @@ gulp.task('sass', function() {
 		app + 'src/landing/primorski/primorski.scss',
 		app + 'src/landing/solomon/solomon.scss',
 		app + 'src/landing/parus/parus.scss',
-		app + 'src/landing/rostservice/rostservice.scss'
+		app + 'src/landing/rostservice/rostservice.scss',
+		app + 'src/landing/burevestnik/burevestnik.scss'
 	])
 		.pipe(plumber(err)) // Отслеживаем ошибки
 		.pipe(cssImport()) // Запускаем @import
